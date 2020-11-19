b922acc4f49d12d16864291ddd8133d672086965
Ted <hellot2@hotmail.com>
2020-11-19 23:05:25 +0800

    get nft endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { createConnection } from "typeorm";
+import { Any, createConnection, getConnectionOptions, Like } from "typeorm";
 import { NFT, NFT_Types } from "./entity/NFT";
 import * as express from "express";
 import { Request, Response } from "express";
@@ -7,7 +7,9 @@ import * as passport from "passport";
 import HeaderAPIKeyStrategy from "passport-headerapikey";
 import { NFTReq } from "./interfaces";
 
+const ENV = process.env.ENV;
 const API_KEY = process.env.API_KEY;
+const DATABASE_URL = process.env.DATABASE_URL;
 
 passport.use(
   new HeaderAPIKeyStrategy(
@@ -23,26 +25,79 @@ passport.use(
   )
 );
 
-createConnection()
-  .then(async (connection) => {
+const createTypeormConn = async () => {
+  const env = ENV == "production" ? ENV : "development";
+  const connectionOptions = await getConnectionOptions(env);
+  return ENV == "production"
+    ? createConnection({
+        ...connectionOptions,
+        url: DATABASE_URL,
+        name: env,
+      } as any)
+    : createConnection({
+        ...connectionOptions,
+        name: env,
+      } as any);
+};
+
+const createServer = async () => {
+  const connection = await createTypeormConn();
+
+  try {
     const nftRepository = connection.getRepository(NFT);
 
     const app = express();
     app.use(express.json());
 
     app.get("/", async (req: Request, res: Response) => {
-      res.send('Solible API');
+      res.send("Solible API");
     });
 
-    app.get("/api/unauthorized", async (req: Request, res: Response) => {
+    app.get("/unauthorized", async (req: Request, res: Response) => {
       res.sendStatus(401);
     });
 
+    app.get("/nft", async (req: Request, res: Response) => {
+      let query = req.query;
+      let condition: any = {};
+      if (query.id) {
+        condition.id = query.id;
+      }
+      let take = query.take ? query.take : 50;
+      let skip = query.skip ? query.skip : 0;
+      condition.name = query.name ? Like("%" + query.name + "%") : Like("%");
+      condition.mintAddress = query.mintAddress
+        ? Like("%" + query.mintAddress + "%")
+        : Like("%");
+      condition.marketAddress = query.marketAddress
+        ? Like("%" + query.marketAddress + "%")
+        : Like("%");
+      condition.redeemAddress = query.redeemAddress
+        ? Like("%" + query.redeemAddress + "%")
+        : Like("%");
+      if (query.type) {
+        condition.type = NFT_Types[query.type.toString()];
+      }
+
+      try {
+        const [result, total] = await nftRepository.findAndCount({
+          where: condition,
+          take: take,
+          skip: skip,
+          order: { id: "DSEC" },
+        } as any);
+
+        res.send(JSON.stringify(result));
+      } catch (error) {
+        res.send([]);
+      }
+    });
+
     app.post(
-      "/api/nft",
+      "/nft",
       passport.authenticate("headerapikey", {
         session: false,
-        failureRedirect: "/api/unauthorized",
+        failureRedirect: "/unauthorized",
       }),
       async (req: Request, res: Response) => {
         let nft = new NFT();
@@ -74,14 +129,14 @@ createConnection()
     );
 
     app.put(
-      "/api/nft",
+      "/nft",
       passport.authenticate("headerapikey", {
         session: false,
-        failureRedirect: "/api/unauthorized",
+        failureRedirect: "/unauthorized",
       }),
       async (req: Request, res: Response) => {
         let body = req.body;
-        let id = body.id;
+        let id = body.id || 0;
         let imgSmall = body.imgSmall;
 
         let nft = await nftRepository.findOne({ id: id });
@@ -105,5 +160,9 @@ createConnection()
     );
 
     app.listen(8000);
-  })
-  .catch((error) => console.log(error));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+createServer();
