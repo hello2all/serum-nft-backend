ecc64ccf8d1d604183a30d896673faf9caa30abe
Ted <hellot2@hotmail.com>
2020-11-19 16:07:49 +0800

    implement endpoints

--- src/entity/NFT.ts
+++ src/entity/NFT.ts
@@ -1,4 +1,5 @@
 import { Entity, PrimaryGeneratedColumn, Column } from "typeorm";
+import { IsNotEmpty, IsInt, IsUrl, IsBoolean, IsArray, IsEnum } from "class-validator";
 
 export enum NFT_Types {
   VIDEO = "VIDEO",
@@ -12,27 +13,35 @@ export class NFT {
   id: number;
 
   @Column()
-  img: string;
-
-  @Column({ default: null, nullable: true })
-  imgSmall: string;
-
-  @Column()
+  @IsNotEmpty()
   name: string;
 
   @Column()
+  @IsUrl()
+  img: string;
+
+  @Column({ default: null, nullable: true })
+  @IsUrl()
+  imgSmall: string;
+
+  @Column()
+  @IsInt()
   supply: number;
 
   @Column()
+  @IsNotEmpty()
   mintAddress: string;
 
   @Column()
+  @IsNotEmpty()
   marketAddress: string;
 
   @Column()
+  @IsBoolean()
   redeemable: boolean;
 
   @Column("simple-array")
+  @IsArray()
   keywords: string[];
 
   @Column({
@@ -40,8 +49,10 @@ export class NFT {
     enum: NFT_Types,
     default: NFT_Types.IMAGE,
   })
+  @IsEnum(NFT_Types)
   type: NFT_Types;
 
   @Column({ default: null, nullable: true })
+  @IsBoolean()
   redeemAddress: string;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,27 @@
-import "reflect-metadata";
 import { createConnection } from "typeorm";
 import { NFT, NFT_Types } from "./entity/NFT";
 import * as express from "express";
 import { Request, Response } from "express";
+import { validate } from "class-validator";
+import * as passport from "passport";
+import HeaderAPIKeyStrategy from "passport-headerapikey";
 import { NFTReq } from "./interfaces";
+
+const API_KEY = process.env.API_KEY;
+
+passport.use(
+  new HeaderAPIKeyStrategy(
+    { header: "Authorization", prefix: "Api-Key " },
+    false,
+    (apikey, done) => {
+      if (apikey === API_KEY) {
+        return done(null, true);
+      } else {
+        return done(null, false);
+      }
+    }
+  )
+);
 
 createConnection()
   .then(async (connection) => {
@@ -12,40 +30,79 @@ createConnection()
     const app = express();
     app.use(express.json());
 
-    app.get("/", (req: Request, res: Response) => {
-      let nft = new NFT();
-      nft.name = 'nft';
-      nft.img = 'test';
-      nft.keywords = ['test1', 'test2'];
-      nft.marketAddress = 'marketaddress';
-      nft.mintAddress = 'mintaddr';
-      nft.redeemAddress = 'redeemaddr';
-      nft.redeemable = false;
-      nft.supply = 1;
-      nft.type = NFT_Types['IMAGE'];
-
-      // res.send("Solible backend");
-      res.send(JSON.stringify(nft));
+    app.get("/", async (req: Request, res: Response) => {
+      res.send('Solible API');
     });
 
-    app.post("/nft", async (req: Request, res: Response) => {
-      let nft = new NFT();
-      let body: NFTReq = req.body;
-      nft.name = body.name;
-      nft.img = body.img;
-      nft.keywords = body.keywords;
-      nft.marketAddress = body.marketAddress;
-      nft.mintAddress = body.mintAddress;
-      if (body.redeemable) {
-        nft.redeemAddress = body.redeemAddress;
-        nft.redeemable = body.redeemable;
+    app.get("/api/unauthorized", async (req: Request, res: Response) => {
+      res.sendStatus(401);
+    });
+
+    app.post(
+      "/api/nft",
+      passport.authenticate("headerapikey", {
+        session: false,
+        failureRedirect: "/api/unauthorized",
+      }),
+      async (req: Request, res: Response) => {
+        let nft = new NFT();
+        let body: NFTReq = req.body;
+        nft.name = body.name;
+        nft.img = body.img;
+        nft.keywords = body.keywords;
+        nft.marketAddress = body.marketAddress;
+        nft.mintAddress = body.mintAddress;
+        if (body.redeemable) {
+          nft.redeemAddress = body.redeemAddress;
+          nft.redeemable = body.redeemable;
+        }
+        nft.supply = body.supply;
+        nft.type = NFT_Types[body.type];
+
+        const errors = await validate(nft);
+        if (errors.length > 0) {
+          res.sendStatus(400);
+        } else {
+          try {
+            await nftRepository.save(nft);
+            res.send(JSON.stringify(nft));
+          } catch (error) {
+            res.sendStatus(500);
+          }
+        }
       }
-      nft.supply = body.supply;
-      nft.type = NFT_Types[body.type];
+    );
 
-      await nftRepository.save(nft);
-      res.send(JSON.stringify(nft));
-    });
+    app.put(
+      "/api/nft",
+      passport.authenticate("headerapikey", {
+        session: false,
+        failureRedirect: "/api/unauthorized",
+      }),
+      async (req: Request, res: Response) => {
+        let body = req.body;
+        let id = body.id;
+        let imgSmall = body.imgSmall;
+
+        let nft = await nftRepository.findOne({ id: id });
+        if (nft === undefined) {
+          res.sendStatus(400);
+          return;
+        }
+        nft.imgSmall = imgSmall;
+        const errors = await validate(nft);
+        if (errors.length > 0) {
+          res.sendStatus(400);
+        } else {
+          try {
+            await nftRepository.save(nft);
+            res.send(JSON.stringify(nft));
+          } catch (error) {
+            res.sendStatus(500);
+          }
+        }
+      }
+    );
 
     app.listen(8000);
   })
